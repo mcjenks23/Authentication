40b58d560d062880980c28f34ed45979277b8cb7
Matthew Jenkins <mcjenks23@gmail.com>
2019-09-04 15:27:36 -0600

    Update Firebase Config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyAWPqfo5ycw82KR72s5fRHTYtf1mEek8h8",
+  authDomain: "authentication-64d08.firebaseapp.com",
+  databaseURL: "https://authentication-64d08.firebaseio.com",
+  projectId: "authentication-64d08",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "404553418409",
+  appId: "1:404553418409:web:694d62963d637722900bb8"
 };
 
 firebase.initializeApp(config);
